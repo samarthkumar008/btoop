701d9ccae83b07fef1e99b595b11a13a88ea6a3a
Akki <92620285+kshatriya16@users.noreply.github.com>
2024-02-10 18:09:04 +0530

    Add files via upload

---
+++ src/core/interfaces/interfaceHeader.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created By: _kshatriya_16
+ * Date: 09/02/2024
+ * Uses:
+ * Last Modefied:
+*/
+
+namespace HttpCode\core\interfaces\interfaceHeader;
+
+interface interfaceHeader{
+  
+  /**
+   * Get All Headers (interface)
+   * @param
+   * @return Array
+  */
+  public static function getallheaders();
+}
+
+/**
+ * END;
+*/
+?>
